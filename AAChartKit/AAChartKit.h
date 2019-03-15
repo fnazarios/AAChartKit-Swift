35ffac662928df78c2b68c7eebe4e5fe2f70d52f
Fernando Sousa <fnazarios@gmail.com>
2019-03-15 17:06:09 -0300

    added carthage compatibility

---
+++ AAChartKit/AAChartKit.h
@@ -0,0 +1,19 @@
+//
+//  AAChartKit.h
+//  AAChartKit
+//
+//  Created by Fernando Sousa on 15/03/19.
+//  Copyright © 2019 An An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AAChartKit.
+FOUNDATION_EXPORT double AAChartKitVersionNumber;
+
+//! Project version string for AAChartKit.
+FOUNDATION_EXPORT const unsigned char AAChartKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AAChartKit/PublicHeader.h>
+
+
